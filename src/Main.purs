01f630708324b4b4b1ed6cc78e5757e18deb193a
Beat Hagenlocher <bah@posteo.de>
2025-06-09 11:53:51 +0200

    Play with type classes

--- src/Main.purs
+++ src/Main.purs
@@ -8,3 +8,17 @@ import Effect.Console (log)
 main :: Effect Unit
 main = do
   log "🍝"
+
+newtype Complex =
+  Complex {
+    real :: Number
+  , imaginary :: Number
+  }
+
+instance showComplex :: Show Complex where
+  show (Complex { real, imaginary }) =
+    case imaginary < 0.0 of
+      true ->
+        show real <> show imaginary <> "i"
+      false ->
+        show real <> "+" <> show imaginary <> "i"
